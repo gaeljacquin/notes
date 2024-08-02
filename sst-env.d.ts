a6a23b5d6cf6830d5dcb1900c274a1ec36bd601b
Gaël Jacquin <8724132+gaeljacquin@users.noreply.github.com>
2024-08-02 17:45:55 -0400

    Adding the API

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -3,17 +3,17 @@
 import "sst"
 declare module "sst" {
   export interface Resource {
-    Api: {
-      type: "sst.aws.ApiGatewayV2"
-      url: string
+    "Api": {
+      "type": "sst.aws.ApiGatewayV2"
+      "url": string
     }
-    Notes: {
-      name: string
-      type: "sst.aws.Dynamo"
+    "Notes": {
+      "name": string
+      "type": "sst.aws.Dynamo"
     }
-    Uploads: {
-      name: string
-      type: "sst.aws.Bucket"
+    "Uploads": {
+      "name": string
+      "type": "sst.aws.Bucket"
     }
   }
 }
